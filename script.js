c4be324b66d7247ee9ca4efa761442c5f31566cb
hector99-Rod <hec2254>
2025-10-20 17:22:15 -0400

    actualizacion datos

--- script.js
+++ script.js
@@ -282,7 +282,7 @@ function actualizarResumen(features) {
     
     resumenDesc.innerHTML = `
         <span style="display: block; margin-bottom: 5px;">
-            Elecciones presidenciales Bolivia 2025. Los datos de segunda vuelta fueron acutalizados a hrs 14:26 del 20/10/2025, cargados al 58,57%.
+            Elecciones presidenciales Bolivia 2025. Los datos de segunda vuelta fueron acutalizados a hrs 17:07 del 20/10/2025, cargados al 68,05%.
         </span>
         <span style="display: block; font-weight: 600;">
             Total General: <strong>${totalGeneral.toLocaleString('es-ES')}</strong>.
